64101ab35b437a9f046861c7dc1f8732a815730f
jrk <jrk>
2022-04-22 13:14:03 +0200

    explicit include path

--- core/init.php
+++ core/init.php
@@ -4,12 +4,12 @@ if (!file_exists('config.php')) {
     die('You\'ve to copy config.php.dist into config.php or execute \'make init\'');
 }
 
-include_once 'constants.php';
+include_once 'core/constants.php';
 
 date_default_timezone_set(APP_TIMEZONE);
 
 include_once 'config.php';
-include_once 'auth.php';
-include_once 'database.php';
+include_once 'core/auth.php';
+include_once 'core/database.php';
 
 ?>
